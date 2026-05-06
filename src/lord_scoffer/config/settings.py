e16ec2ce35e3bd4bccce3c21bca77251928a86c1
Julio Silveira <silveira.julio@gmai.com>
2026-05-06 20:22:36 -0300

    :rocket: Added config settings.py

---
+++ src/lord_scoffer/config/settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+    MODEL_NAME: str = "gpt-4o-mini"
+    TEMPERATURE: float = 1.2
+
+    TWILIO_ACCOUNT_SID: str = os.getenv("TWILIO_ACCOUNT_SID", "")
+    TWILIO_AUTH_TOKEN: str = os.getenv("TWILIO_AUTH_TOKEN", "")
+    TWILIO_PHONE_NUMBER: str = os.getenv("TWILIO_PHONE_NUMBER", "")
+
+settings = Settings()
